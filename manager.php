6bbd74d4864ff6d9bfeeb25a19bc544624a4512e
Gago <gagmuradyan93@gmail.com>
2019-06-20 15:01:45 +0400

    Add hub & spoke files boilerplate

--- manager.php
+++ manager.php
@@ -6,7 +6,9 @@
  */
 
 /* Require plug-in files */
-require_once __DIR__ . '/includes/woocommerce-hub.php';
+require_once __DIR__ . '/includes/add-on-hub.php';
+require_once __DIR__ . '/includes/add-on-spoke.php';
 
 /* Call the setup functions */
-\Distributor\WoocommerceHub\setup();
+\Distributor\AddOnHub\setup();
+\Distributor\AddOnSpoke\setup();
